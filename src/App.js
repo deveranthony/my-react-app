dc3b5c7c7f090af69a3093ad11c48218b299c3ee
deveranthony <deveranthony5@gmail.com>
2025-09-02 07:25:55 -1100

    this is my first react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import { MyComponent, AnotherComponent } from './components/components'
 import Mycomo from './components/mycom';
+import MainContent from './components/myco';
+import { Header,Footer } from './components/myco';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <MyComponent/>
+      <MainContent/>
       <AnotherComponent/>
       <Mycomo/>
+      <Footer/> 
     </div>
   );
 }
---
+++ src/components/myco.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+    return <h1>Welcome to My Website</h1>;
+};
+
+export const Footer = () => {
+    return <footer>© 2023 My Website</footer>;
+};
+
+export default function MainContent() {
+    return <div>This is the main content of the website.</div>;
+};

0002378e0f2e214d012cd76da7ed33274f2b9855
deveranthony <deveranthony5@gmail.com>
2025-09-07 03:56:52 -1100

    This is button

--- src/components/components.js
+++
@@ -1,7 +0,0 @@
-export const MyComponent = () => {
-    return <h1>Hello from MyComponent!</h1>;
-};
-
-export const AnotherComponent = () => {
-    return <h1>Hello from AnotherComponent!</h1>;
-};
--- src/components/mycom.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Mycomo = () => {
-    return<h1>Hello from Mycomo!</h1>;
-};
-
-export default Mycomo;
